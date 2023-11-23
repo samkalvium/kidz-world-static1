a8e2895f1056f950da18ad33fb23746665be98e4
samkalvium <samarth.s@kalvium.community>
2023-11-23 10:45:33 +0530

    final push

--- app.js
+++ app.js
@@ -3,20 +3,62 @@ const cartValue = document.getElementById("cart-value");
 const addButtons = document.getElementsByClassName("button");
 
 const items = [
-  { name: "This was our pact", quantity: 0, dollars: 7, cents: 49 },
-  { name: "The famous five", quantity: 0, dollars: 4, cents: 59 },
-  { name: "Matilda", quantity: 0, dollars: 6, cents: 80 },
-  { name: "Harry Potter",quantity: 0,dollars: 10,cents: 0,},
-  { name: "For Young Readers",quantity: 0,dollars: 7,cents: 29,},
-  {name: "Wimpy Kid - DIY",quantity: 0,dollars: 4,cents: 99,},
-  {name: "Dart Board",quantity: 0, dollars: 17, cents: 49,},
-  {name: "Connect Four", quantity: 0, dollars: 19,cents: 99,},
-  { name: "Jenga", quantity: 0, dollars: 20,cents: 99,},
-  { name: "Monopoly", quantity: 0,  dollars: 19, cents: 49,},
-  { name: "Bookmarks", quantity: 0, dollars: 12, cents: 49, },
-  { name: "Birthday Card",quantity: 0, dollars: 12,cents: 49, },
-  { name: "Stuffed toys",quantity: 0, dollars: 15,  cents: 99, },
-  { name: "Dream catcher drawing", quantity: 0,dollars: 18, cents: 49,},
+  {
+    name: "This was our pact",
+    quantity: 0, dollars: 7, cents: 49
+  },
+  {
+    name: "The famous five",
+    quantity: 0, dollars: 4, cents: 59
+  },
+  {
+    name: "Matilda",
+    quantity: 0, dollars: 6, cents: 80
+  },
+  {
+    name: "Harry Potter",
+    quantity: 0, dollars: 10, cents: 0,
+  },
+  {
+    name: "For Young Readers",
+    quantity: 0, dollars: 7, cents: 29,
+  },
+  {
+    name: "Wimpy Kid - DIY",
+    quantity: 0, dollars: 4, cents: 99,
+  },
+  {
+    name: "Dart Board",
+    quantity: 0, dollars: 17, cents: 49,
+  },
+  {
+    name: "Connect Four",
+    quantity: 0, dollars: 19, cents: 99,
+  },
+  {
+    name: "Jenga",
+    quantity: 0, dollars: 20, cents: 99,
+  },
+  {
+    name: "Monopoly",
+    quantity: 0, dollars: 19, cents: 49,
+  },
+  {
+    name: "Bookmarks",
+    quantity: 0, dollars: 12, cents: 49,
+  },
+  {
+    name: "Birthday Card",
+    quantity: 0, dollars: 12, cents: 49,
+  },
+  {
+    name: "Stuffed toys",
+    quantity: 0, dollars: 15, cents: 99,
+  },
+  {
+    name: "Dream catcher drawing",
+    quantity: 0, dollars: 18, cents: 49,
+  },
 ];
 
 // Update the cart display
